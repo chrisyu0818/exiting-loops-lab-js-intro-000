f124f9a50a63548c9feb14d1a751c40b4b457b9a
Zhenhua yu <zyu0628@gmail.com>
2020-10-14 17:47:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,10 @@ function keepGoing(array, changeValue, skipValue){
     }array[i]=changeValue
   }return array
 }
+function findBy(array, findFn){
+  for(let i=0; i<array.length; i++) {
+    if(findFn(array[i])){
+      return array[i]
+    }
+  }return null
+}
