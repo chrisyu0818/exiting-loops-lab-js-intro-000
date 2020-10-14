845f28cc206ee1e030e95a0b6a094a3643afcd3a
Zhenhua yu <zyu0628@gmail.com>
2020-10-14 17:35:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function breakOut(array, changeValue, stopValue) {
   for (let i=0; i<array.length; i++) {
-    if (changeValue[i]===stopValue[i]){
+    if (changeValue[i]===stopValue){
       break
     }return array
   }
